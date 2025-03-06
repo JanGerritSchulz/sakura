19ed13bdabb179932750643aa36ccbe59a477275
Jan Schulz (LXPLUS) <jan.gerrit.schulz@cern.ch>
2025-03-06 15:17:18 +0100

    Add missing general plots to the makeGeneralPlots function

--- simplotter/makeGeneralPlots.py
+++ simplotter/makeGeneralPlots.py
@@ -21,7 +21,6 @@ def plotLayerPairs(ROOTfile, directory="plots", num_events=None):
     else:
         ylabel_suff = " / event"
     
-    
     # load histograms
     h = Hist2D(ROOTfile["general"], "layerPairs", "Outer layer ID", "Inner layer ID", scale_for_values = 1/num_events)
     
@@ -76,6 +75,78 @@ def plotLayerPairs(ROOTfile, directory="plots", num_events=None):
 
 # ------------------------------------------------------------------------------------------
 
+def plotDiscreteHist(ROOTfile, histname, directory="plots", num_events=None, x_label="", y_label="", x_lim=(None, None)):
+    if num_events is None:
+        num_events = 1
+        ylabel_suff = ""
+    else:
+        ylabel_suff = " / event"
+
+    
+    # load histograms
+    hTot = Hist(ROOTfile, "general/%s" % (histname), scale_for_values = 1/num_events)
+    hPass = Hist(ROOTfile, "general/pass_%s" % (histname), scale_for_values = 1/num_events)
+    
+    # create new figure
+    fig, ax = plt.subplots()
+    
+    # plot
+    y = hTot.values
+    x = (hTot.edges[1:] + hTot.edges[:-1])/2
+    xTot = x - (hTot.edges[1:]-hTot.edges[:-1]) / 6
+    xPass = x + (hTot.edges[1:]-hTot.edges[:-1]) / 6
+    
+    plt.bar(xTot, hTot.values, 1/3, color='darkblue', label="total")
+    plt.bar(xPass, hPass.values, 1/3, color='#5790fc', label="passing")
+    plt.legend()
+
+    # fix axes
+    plt.xticks(x, [(str(int(x_)) if (x_%1==0) else "") for x_ in x])
+    ax.xaxis.set_tick_params(which='minor',bottom=False,top=False)
+    ylabel(y_label + ylabel_suff)
+    xlabel(x_label)
+    ax.set_xlim(x_lim)
+    if "pT" in histname:
+        plt.xscale("log")
+    
+    # add the CMS label
+    cmslabel(llabel="Private Work", com=14)
+    
+    # save and show the figure
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    savefig("%s/%s.png" % (directory, histname))
+    plt.close()
+
+# ------------------------------------------------------------------------------------------
+
+def plotEfficiency(ROOTfile, histname, directory="plots", x_label="", y_label=""):
+    
+    # load histograms
+    hist = Hist(ROOTfile, "general/%s" % (histname))
+    
+    # create new figure
+    fig, ax = plt.subplots()
+    
+    # plot    
+    hist.plot(marker="s")
+
+    # fix axes
+    ylabel(y_label)
+    xlabel(x_label)
+    plt.ylim(0,1)
+    if "pT" in histname:
+        plt.xscale("log")
+    
+    # add the CMS label
+    cmslabel(llabel="Private Work", com=14)
+    
+    # save and show the figure
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    savefig("%s/%s.png" % (directory, histname))
+    plt.close()
+
+# ------------------------------------------------------------------------------------------
+
 # define function that performs the plotting of all cuts
 def makeGeneralPlots(DQMfile, DIR="plots", num_events=None):
 
@@ -87,6 +158,31 @@ def makeGeneralPlots(DQMfile, DIR="plots", num_events=None):
     # plot the layerPairs
     plotLayerPairs(ROOTFile, directory=DIR, num_events=num_events)
 
+    # produce discrete distributions
+    discreteDict = {
+        "numSimDoubletsPerTrackingParticle" : {"x_label" : "Number of SimDoublets per TrackingParticle", "y_label" : "Number of TrackingParticles", "x_lim": (-0.5, 10.5)},
+        "numLayersPerTrackingParticle" : {"x_label" : "Number of hit layers per TrackingParticle", "y_label" : "Number of TrackingParticles", "x_lim": (-0.5, 10.5)},
+        "numSkippedLayers" : {"x_label" : "Number of skipped layers", "y_label" : "Number of SimDoublets", "x_lim": (-1.5, 5.5)},
+    }
+    for h in discreteDict.keys():
+        p = discreteDict[h]
+        plotDiscreteHist(ROOTFile, h, directory=DIR, num_events=num_events, 
+                         x_label=p["x_label"], y_label=p["y_label"], x_lim=p["x_lim"])
+        
+    
+    # produce efficiency plots
+    efficiencyDict = {
+        "efficiencyPerTP_vs_eta" : {"x_label" : "TrackingParticle pseudorapidity $\eta$", "y_label" : "Average fraction of SimDoublets \nper TrackingParticle passing all cuts"},
+        "efficiencyPerTP_vs_pT" : {"x_label" : r"TrackingParticle transverse momentum $p_\text{T}$ [GeV]", "y_label" : "Average fraction of SimDoublets \nper TrackingParticle passing all cuts"},
+        "efficiencyTP_vs_eta" : {"x_label" : "TrackingParticle pseudorapidity $\eta$", "y_label" : "Efficiency for TrackingParticles \n(at least 2 passing and connected SimDoublets)"},
+        "efficiencyTP_vs_pT" : {"x_label" : r"TrackingParticle transverse momentum $p_\text{T}$ [GeV]", "y_label" : "Efficiency for TrackingParticles \n(at least 2 passing and connected SimDoublets)"},
+        "efficiency_vs_eta" : {"x_label" : "TrackingParticle pseudorapidity $\eta$", "y_label" : "Total fraction of SimDoublets passing all cuts"},
+        "efficiency_vs_pT" : {"x_label" : r"TrackingParticle transverse momentum $p_\text{T}$ [GeV]", "y_label" : "Total fraction of SimDoublets passing all cuts"},
+    }
+    for h in efficiencyDict.keys():
+        p = efficiencyDict[h]
+        plotEfficiency(ROOTFile, h, directory=DIR, 
+                         x_label=p["x_label"], y_label=p["y_label"])
 
 
 
