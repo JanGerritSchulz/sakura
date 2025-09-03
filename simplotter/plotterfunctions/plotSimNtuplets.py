a2d24dedb2deeb913db053ff377566e15ba0ba8b
Jan Schulz <jan.gerrit.schulz@cern.ch>
2025-09-03 12:20:04 +0200

    introduce cut on triplets in SimNtuplet plot

--- simplotter/plotterfunctions/plotSimNtuplets.py
+++ simplotter/plotterfunctions/plotSimNtuplets.py
@@ -4,9 +4,9 @@ from simplotter.utils.histtools import getHist
 from simplotter.utils.plotttools import cmslabel, savefig
 
 
-def plotSimNtuplets(rootFile, plotConfig, directory="plots", cmsConfig=None, saveas="png"):
+def plotSimNtuplets(rootFile, plotConfig, directory="plots", cmsConfig=None, saveas="png", rootFileMTV=None):
 
-    colorPalette1 = ["#C0FB2D", "#1B2021", "#016FB9", "#61E8E1", "#336346", "#88958D", "#ff00ff", "#ff00ff", "#DEDEDE"]
+    colorPalette1 = ["#C0FB2D", "#1B2021", "#016FB9", "#61E8E1", "#773FF8", "#336346", "#88958D", "#ff00ff", "#ff00ff", "#DEDEDE"]
     colorPalette2 = ["#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000", "#FFB000", "#ff00ff", "#ff00ff", "#DEDEDE"]
 
     xQuantity = "eta" if "eta" in plotConfig.xLabel else "pt"
@@ -16,13 +16,14 @@ def plotSimNtuplets(rootFile, plotConfig, directory="plots", cmsConfig=None, sav
     # load histograms
     categories = {
         "Alive" : {"label" : "built", "color" : colorPalette[0]},
-        "NotStartingPair" : {"label" : "Ntuplet starts not in a starting pair", "color" : colorPalette[4]},
-        "KilledConnections" : {"label" : "has killed connections", "color" : colorPalette[3]},
+        "NotStartingPair" : {"label" : "Ntuplet does not start in a starting pair", "color" : colorPalette[5]},
+        "KilledTripletConnections" : {"label" : "has killed triplet connections", "color" : colorPalette[4]},
+        "KilledDoubletConnections" : {"label" : "has killed doublet connections", "color" : colorPalette[3]},
         "KilledDoublets" : {"label" : "has killed doublets", "color" : colorPalette[2]},
         "MissingLayerPair" : {"label" : "is missing a layer pair", "color" : colorPalette[1]},
-        "TooShort" : {"label" : "shorter than reco threshold", "color" : colorPalette[5]},
-        "UndefDoubletCuts" : {"label" : "has undef doublet cuts", "color" : colorPalette[6]},
-        "UndefConnectionCuts" : {"label" : "has undef connection cuts", "color" : colorPalette[7]},
+        "TooShort" : {"label" : "shorter than reco threshold", "color" : colorPalette[6]},
+        "UndefDoubletCuts" : {"label" : "has undef doublet cuts", "color" : colorPalette[7]},
+        "UndefConnectionCuts" : {"label" : "has undef connection cuts", "color" : colorPalette[8]},
     }
     hists = {
         c : getHist(rootFile, "SimPixelTracks/SimNtuplets/%s/frac%s_vs_%s" % (plotConfig.histname, c, xQuantity)) for c in categories
@@ -51,8 +52,12 @@ def plotSimNtuplets(rootFile, plotConfig, directory="plots", cmsConfig=None, sav
     ax.stairs(np.ones_like(hists[list(categories.keys())[0]].values()), 
               edges, baseline=y_baseline, 
                 label="has 2 or less RecHits",
-                edgecolor=colorPalette[8], hatch='//')
+                edgecolor=colorPalette[9], hatch='//')
     
+    if rootFileMTV is not None:
+        recoEff = getHist(rootFileMTV, "hltPhase2Pixel_hltAssociatorByHits/effic")
+        recoEff.plot1D(ax=ax, histtype="errorbar", color="#FF0000", fmt=".", label="actual RecoTrack efficiency")
+
     ax.legend(title="Status of TP's %s SimNtuplet" % ("longest" if plotConfig.histname=="longest" else "most alive"),
                reverse=True, loc='center left', bbox_to_anchor = (1.03, 0.5))
 
