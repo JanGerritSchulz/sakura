935ad43ecb63e125424a49bdcbcf63b442d83f35
Jan Schulz <jan@fiveschulz.de>
2025-11-12 10:14:37 +0900

    Fix vmax and extend of plot2D

--- simplotter/plotterfunctions/plot2D.py
+++ simplotter/plotterfunctions/plot2D.py
@@ -2,7 +2,7 @@ import matplotlib as mpl
 from matplotlib.colors import Normalize, LogNorm
 import numpy as np
 
-def plot2D_(theHist, fig, ax, zLabel="", logZ=False, nEvents=None, cmap="plasma"):
+def plot2D_(theHist, fig, ax, zLabel="", logZ=False, nEvents=None, cmap="plasma", vmax=None):
     # get histogram data
     w, x, y = theHist.to_numpy()
     if nEvents is not None:
@@ -10,13 +10,13 @@ def plot2D_(theHist, fig, ax, zLabel="", logZ=False, nEvents=None, cmap="plasma"
 
     # set limits
     vmin = np.nanmin(np.ma.masked_equal(w, 0.0, copy=False)) * 0.999
-    vmax = np.nanmax(w)
+    vmax = np.nanmax(w) if vmax is None else vmax
     if logZ:
         ax.pcolormesh(x, y, w.T, norm=LogNorm(vmin=vmin, vmax=vmax), cmap=cmap)
         fig.colorbar(mpl.cm.ScalarMappable(norm=LogNorm(vmin=vmin, vmax=vmax), cmap=cmap), 
                     ax=ax, extend='min', label=zLabel + ("" if nEvents is None else " / event"))
 
     else:
-        ax.pcolormesh(x, y, w.T, cmap=cmap)
+        ax.pcolormesh(x, y, w.T, norm=Normalize(vmin=vmin, vmax=vmax), cmap=cmap)
         fig.colorbar(mpl.cm.ScalarMappable(norm=Normalize(vmin=vmin, vmax=vmax),cmap=cmap), 
-                    ax=ax, label=zLabel)
+                    ax=ax, extend='min', label=zLabel)
